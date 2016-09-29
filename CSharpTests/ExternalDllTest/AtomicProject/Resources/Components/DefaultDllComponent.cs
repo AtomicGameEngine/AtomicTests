9e232fbd104fdcf2719d7969f052e165f3dbd1d0
Matt Benic <mattbenic@gmail.com>
2016-09-29 08:51:30 +0200

    Add CSharpExternalDllTest
    Tests adding and accessing components in dlls other than the default generated by AtomicNET

---
+++ CSharpTests/ExternalDllTest/AtomicProject/Resources/Components/DefaultDllComponent.cs
@@ -0,0 +1,14 @@
+using System;
+using AtomicEngine;
+
+public class DefaultDllComponent : CSComponent
+{
+    [Inspector]
+    public int MyIntValue;
+
+    void Update(float timeStep)
+    {
+
+    }
+
+}
---
+++ CSharpTests/ExternalDllTest/AtomicProject/Resources/Components/DefaultDllDerivedComponent.cs
@@ -0,0 +1,14 @@
+using System;
+using AtomicEngine;
+
+public class DefaultDllDerivedComponent : DefaultDllComponent
+{
+    [Inspector]
+    public float MyFloatValue;
+
+    void Update(float timeStep)
+    {
+
+    }
+
+}
