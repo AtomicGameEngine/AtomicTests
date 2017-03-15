d3baf984269c02999a96fa1bd01c593d7bc9eab0
Josh Engebretson <josh@galaxyfarfaraway.com>
2017-03-15 13:16:30 -0700

    Adding SceneResource test

--- CSharpTests/SceneResourcesTest/Resources/Components/Spinner.cs
+++ CSharpTests/SceneResourcesTest/Resources/Components/Spinner.cs
@@ -5,11 +5,21 @@ public class Spinner : CSComponent
 {
 
     [Inspector]
-    float speed = 1.0f;    
+    float speed = 1.0f;
+
+    Node node;
+
+    void Start()
+    {
+        // store a local reference to Node, otherwise C# wrapper around native instance created and GC'd
+        // when running GC aggressively, this causes a bunch of ScriptVectors to be created for return values
+        // in instance
+        node = Node;
+    }
 
     void Update(float timeStep)
     {
-        Node.Yaw(speed * timeStep * 75.0f);
+        node.Yaw(speed * timeStep * 75.0f);
     }
 
 }
--- CSharpTests/SceneResourcesTest/Resources/Scripts/AtomicMain.cs
+++ CSharpTests/SceneResourcesTest/Resources/Scripts/AtomicMain.cs
@@ -1,11 +1,54 @@
+using System;
 using AtomicEngine;
 using AtomicPlayer;
 
 public class AtomicMain : AppDelegate
 {
+    // scene switch time in seconds
+    float switchTime = 2.0f;
+
+    int currentScene = 0;
+    string[] scenes = { "Scene", "Scene2", "EmptyScene" };
+
     public override void Start()
     {
-        AtomicNET.GetSubsystem<Player>().LoadScene("Scenes/Scene.scene");
+        var ui = GetSubsystem<UI>();
+
+        // set up the DebugHud to show metrics and update at 10hz
+        ui.SetDebugHudProfileMode(DebugHudProfileMode.DEBUG_HUD_PROFILE_METRICS);
+        ui.ShowDebugHud(true);
+        ui.DebugHudRefreshInterval = 0.1f;
+
+        switchScene();
+
+        SubscribeToEvent<UpdateEvent>(e =>
+        {
+
+            switchTime -= e.TimeStep;
+
+            if (switchTime <= 0.0f)
+            {
+                switchTime = 5.0f;
+
+                switchScene();
+            }
+
+        });
+    }
+
+    void switchScene()
+    {
+        var player = GetSubsystem<Player>();
+
+        player.UnloadAllScenes();
+
+        // unload resources from cache
+        GetSubsystem<ResourceCache>().ReleaseAllResources(false);
+
+        player.LoadScene("Scenes/" + scenes[currentScene++] + ".scene");
+
+        currentScene %= scenes.Length;
+
     }
 
 }
