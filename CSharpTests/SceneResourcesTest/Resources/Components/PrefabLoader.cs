fb67460326d01321f4a7220c124ac3f9142d20c2
Matt Benic <mattbenic@gmail.com>
2017-04-05 16:40:44 +0200

    Added code for class that somehow ended up in the project dir

--- CSharpTests/SceneResourcesTest/Resources/Components/PrefabLoader.cs
+++ CSharpTests/SceneResourcesTest/Resources/Components/PrefabLoader.cs
@@ -1,3 +1,25 @@
-
 using System;
+
 using AtomicEngine;
+
+public class PrefabLoader : CSComponent
+{
+    [Inspector]
+    private string prefabPath;
+
+    public void Start()
+    {
+        if (!string.IsNullOrEmpty(prefabPath))
+        {
+            LoadPrefab();
+        }
+    }
+
+    private void LoadPrefab()
+    {
+        Node prefabNode = Node.CreateChild();
+        PrefabComponent prefabComponent = prefabNode.CreateComponent<PrefabComponent>();
+        prefabComponent.SetPrefabGUID(prefabPath);
+        Node.AddChild(prefabComponent.GetNode());
+    }
+}
